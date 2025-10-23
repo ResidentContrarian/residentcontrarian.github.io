9daa2f163fc36c90c6a71ec6dbdb27327fc5c112
ResidentContrarian <110250615+ResidentContrarian@users.noreply.github.com>
2025-10-22 21:14:15 -0700

    Update authors.js

--- pages/api/authors.js
+++ pages/api/authors.js
@@ -1,24 +1,7 @@
-import { useEffect, useState } from "react";
-
-export default function AuthorsPage() {
-  const [authors, setAuthors] = useState([]);
-
-  useEffect(() => {
-    fetch("/api/get-authors")
-      .then(res => res.json())
-      .then(data => setAuthors(data));
-  }, []);
-
-  return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Authors</h1>
-      <ul>
-        {authors.map(author => (
-          <li key={author.id}>
-            <a href={author.url}>{author.name}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+// pages/api/authors.js
+export default function handler(req, res) {
+  res.status(200).json([
+    { id: 1, name: "Jane Doe" },
+    { id: 2, name: "John Smith" }
+  ]);
 }
