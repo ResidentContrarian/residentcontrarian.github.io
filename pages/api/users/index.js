7297f4537ae69ebf5ee8b9bef76ae22520e39afd
ResidentContrarian <110250615+ResidentContrarian@users.noreply.github.com>
2025-10-23 12:33:49 -0700

    Delete pages/api/users directory

--- pages/api/users/index.js
+++
@@ -1,6 +0,0 @@
-// pages/api/users/index.js
-export default function handler(req, res) {
-  res.statusCode = 200;
-  res.setHeader('content-type', 'text/plain; charset=utf-8');
-  res.end('users route OK');
-}
