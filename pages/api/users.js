0317bdea7808a6e42525c12b390a96627bfbbc98
ResidentContrarian <110250615+ResidentContrarian@users.noreply.github.com>
2025-10-24 07:28:53 -0700

    Delete pages directory

--- pages/api/users.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.statusCode = 200;
-  res.setHeader('content-type', 'text/plain; charset=utf-8');
-  res.end('users flat file OK');
-}
--- pages/index.js
+++
@@ -1,5 +0,0 @@
-const resp = await fetch('/api/user-create', {
-  method: 'POST',
-  headers: { 'content-type':'application/json' },
-  body: JSON.stringify({ username: uname })
-});
